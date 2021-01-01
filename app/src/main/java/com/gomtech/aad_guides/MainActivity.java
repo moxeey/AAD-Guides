526fbbbeee40e23e6db975a3f78e022713acb33d
mz_ahmad <muhdzubairuahmad@gmail.com>
2021-01-01 23:59:45 +0100

    toast

--- app/src/main/java/com/gomtech/aad_guides/MainActivity.java
+++ app/src/main/java/com/gomtech/aad_guides/MainActivity.java
@@ -3,12 +3,38 @@ package com.gomtech.aad_guides;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnToast ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnToast = findViewById(R.id.btn_toast);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.custom_toast,
+                        (ViewGroup) findViewById(R.id.custom_toast_container));
+
+                TextView text = (TextView) layout.findViewById(R.id.text);
+                text.setText("Hello");
+//
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.TOP|Gravity.LEFT, 0,0);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setView(layout);
+                toast.show();
+            }
+        });
     }
 }
